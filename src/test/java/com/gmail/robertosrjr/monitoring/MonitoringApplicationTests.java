e74b9d9353b4ee510674888b0a1a720892d35f6e
Roberto S. Ramos Jr <robertosrjr@gmail.com>
2020-07-11 18:54:29 -0300

    monitoramento

---
+++ src/test/java/com/gmail/robertosrjr/monitoring/MonitoringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gmail.robertosrjr.monitoring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MonitoringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
